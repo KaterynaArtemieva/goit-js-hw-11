78028862d7ae4f206e2f355f077660fd00f85979
Kateryna Artemieva <moruga.katya@gmail.com>
2023-04-28 15:45:18 +0100

    commit

--- src/fetchImages.js
+++ src/fetchImages.js
@@ -1,12 +1,12 @@
-export function fetchImages(name) {
+import axios from 'axios';
+const axios = require('axios');
+
+export async function fetchImages(nameImg, page) {
   const BASE_URL = 'https://pixabay.com/api/';
-  // const END_POINT = 'name/';
-  // const FILTER = '?fields=name,capital,population,flags,languages';
-  const API_KEY = "35829510-9a51be363aad92e9acd99befc";
-  return fetch(`${BASE_URL}${END_POINT}${name}${FILTER}`).then(resp => {
-    if (!resp.ok) {
-      throw new Error(resp.statusText);
-    }
-    return resp.json();
-  });
+  const API_KEY = '35829510-9a51be363aad92e9acd99befc';
+  let res = await axios.get(
+    `${BASE_URL}?key=${API_KEY}&q=${nameImg}&image_type=photo&orientation=horisontal&safesearch=true&page=${page}&per_page=40`
+  );
+  let data = res.data;
+  return data;
 }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,119 @@
 import './css/styles.css';
 import Notiflix from 'notiflix';
 import { fetchImages } from './fetchImages';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const form = document.querySelector(".search-form");
-const gallery = document.querySelector(".gallery");
+const form = document.querySelector('.search-form');
+const gallery = document.querySelector('.gallery');
+const target = document.querySelector('.guard');
+let searchImg = '';
+let currentPage = 1;
+let options = {
+  root: null,
+  rootMargin: '300px',
+  threshold: 1.0,
+};
+form.addEventListener('submit', onFormSubmit);
 
-form.addEventListener("submit", onFormSubmit);
-
-function onFormSubmit(evt){
+function onFormSubmit(evt) {
   evt.preventDefault();
+  gallery.innerHTML = '';
+  let currentPage = 1;
   const {
     elements: { searchQuery },
   } = evt.currentTarget;
-const searchImg = searchQuery.value.trim();
-if(searchImg){
-  fetchImages(searchImg)
+  searchImg = searchQuery.value.trim();
+  if (searchImg) {
+    fetchImages(searchImg, currentPage)
+      .then(data => {
+        if (data.total) {
+          gallery.insertAdjacentHTML('beforeend', createMarkup(data));
+          Notiflix.Notify.success(`Hooray! We found ${data.total} images.`);
+          let observer = new IntersectionObserver(onLoad, options);
+          observer.observe(target);
+          
+        } else {
+          Notiflix.Notify.failure(
+            'Sorry, there are no images matching your search query. Please try again.'
+          );
+          const {
+            elements: { searchQuery },
+          } = form;
+          searchQuery.value = '';
+        }
+      })
+      .catch(err);
+  }
 }
 
+function onLoad(entries, observer, searchImg) {
+    entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      currentPage += 1;
+      const {
+        elements: { searchQuery },
+      } = form;
+      searchImg = searchQuery.value.trim();
+      fetchImages(searchImg, currentPage)
+        .then(data => {
+          gallery.insertAdjacentHTML('beforeend', createMarkup(data));
+          const { height: cardHeight } = document
+            .querySelector('.gallery')
+            .firstElementChild.getBoundingClientRect();
 
+          window.scrollBy({
+            top: cardHeight * 2,
+            behavior: 'smooth',
+          });
+          let totalPageImg = 40 * currentPage;
+          if (totalPageImg >= data.total) {
+            observer.unobserve(target);
+            Notiflix.Notify.info(
+              "We're sorry, but you've reached the end of search results."
+            );
+          }
+        })
+        .catch(err => console.log(err));
+    }
+  });
+}
 
+function createMarkup(data) {
+  let arr = data.hits;
+  return arr
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `<div class="photo-card">
+      <a class="gallery__link" href="${largeImageURL}">
+      <img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy"/>
+      <div class="info">
+        <p class="info-item">
+          <b>Likes: ${likes}</b>
+        </p>
+        <p class="info-item">
+          <b>Views: ${views}</b>
+        </p>
+        <p class="info-item">
+          <b>Comments: ${comments}</b>
+        </p>
+        <p class="info-item">
+          <b>Downloads: ${downloads}</b>
+        </p>
+      </div></a>
+    </div>
+  `
+    )
+    .join('');
+}
+
+function err(error) {
+  Notiflix.Notify.failure(`${error.message}`);
 }
