a8053151be5b28b0e29ea560b1e2d1d3c4e1f088
Kateryna Artemieva <moruga.katya@gmail.com>
2023-04-28 22:37:05 +0100

    axios

--- src/fetchImages.js
+++ src/fetchImages.js
@@ -1,22 +1,8 @@
 import axios from 'axios';
-// const axios = require('axios').default;
 export async function fetchImages(nameImg, page) {
   const BASE_URL = 'https://pixabay.com/api/';
   const API_KEY = '35829510-9a51be363aad92e9acd99befc';
-  try {
-    const res = await axios.get(`${BASE_URL}?key=${API_KEY}&q=${nameImg}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`);
-    let data = res.data;
-    return data;
-    } catch (error) {
-    console.error(error);
-  }
+  let res = await axios.get(`${BASE_URL}?key=${API_KEY}&q=${nameImg}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`);
+  let data = res.data;
+  return data;
 }
-
-
-// export async function fetchImages(nameImg, page) {
-//   const BASE_URL = 'https://pixabay.com/api/';
-//   const API_KEY = '35829510-9a51be363aad92e9acd99befc';
-//   let res = await axios.get(`${BASE_URL}?key=${API_KEY}&q=${nameImg}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`);
-//   let data = res.data;
-//   return data;
-// }
