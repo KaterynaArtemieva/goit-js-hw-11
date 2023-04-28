4683033d75c7b9c86584449ec3f9a0cbb878e2fd
Kateryna Artemieva <moruga.katya@gmail.com>
2023-04-28 15:49:03 +0100

    hbj

--- src/fetchImages.js
+++ src/fetchImages.js
@@ -4,8 +4,7 @@ const axios = require('axios');
 export async function fetchImages(nameImg, page) {
   const BASE_URL = 'https://pixabay.com/api/';
   const API_KEY = '35829510-9a51be363aad92e9acd99befc';
-  let res = await axios.get(`${BASE_URL}?key=${API_KEY}&q=${nameImg}&image_type=photo&orientation=horisontal&safesearch=true&page=${page}&per_page=40`
-  );
+  let res = await axios.get(`${BASE_URL}?key=${API_KEY}&q=${nameImg}&image_type=photo&orientation=horisontal&safesearch=true&page=${page}&per_page=40`);
   let data = res.data;
   return data;
 }
